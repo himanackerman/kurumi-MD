e1a9e59ac9743557c7faee1302f11b6efb55c772
Hilman <himanackerman@gmail.com>
2026-05-30 15:55:04 +0700

    Anu

--- plugins/down-sfile.js
+++ plugins/down-sfile.js
@@ -1,70 +1,21 @@
-import * as cheerio from 'cheerio';
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text }) => {
-	if (text.match(/(https:\/\/sfile.mobi\/)/gi)) {
-		let res = await download(text);
-		if (!res) throw 'Tidak Dapat Mengunduh File';
-		await m.reply(
-			Object.keys(res)
-				.map((v) => `*• ${v.capitalize()}:* ${res[v]}`)
-				.join('\n') + '\n\n_Sending file..._'
-		);
-		const buff = Buffer.from(await (await fetch(res.download)).arrayBuffer());
-		conn.sendMessage(m.chat, { document: buff, fileName: res.filename, mimetype: res.mimetype }, { quoted: m });
-	} else if (text) {
-		let [query, page] = text.split`|`;
-		let res = await search(query, page);
-		if (!res.length) throw `Query "${text}" not found :/`;
-		res = res.map((v) => `*Title:* ${v.title}\n*Size:* ${v.size}\n*Link:* ${v.link}`).join`\n\n`;
-		m.reply(res);
-	} else return m.reply('Input Query / Sfile Url!');
-};
-handler.help = ['sfile'];
-handler.tags = ['downloader'];
-handler.command = /^(sfile)$/i;
-handler.limit = true;
-export default handler;
-
-async function search(query, page = 1) {
-	const res = await fetch(`https://sfile.mobi/search.php?q=${query}&page=${page}`);
-	const $ = cheerio.load(await res.text());
-	const result = [];
-	$('div.list').each(function () {
-		const title = $(this).find('a').text();
-		const size = $(this).text().trim().split('(')[1];
-		const link = $(this).find('a').attr('href');
-		if (link) result.push({ title, size: size.replace(')', ''), link });
-	});
-
-	return result;
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw 'Masukkan URL sfile.mobi!'
+try {
+let res = await fetch('https://api.nexray.eu.cc/downloader/sfile?url=' + encodeURIComponent(text))
+let json = await res.json()
+if (!json.status) throw 'Gagal mengambil data'
+let { file_name, size, url, author_name, upload_date } = json.result
+let caption = 'Nama: ' + file_name + '\nUkuran: ' + size + '\nUpload: ' + upload_date + '\nAuthor: ' + author_name
+await conn.reply(m.chat, caption, m)
+await conn.sendMessage(m.chat, { document: { url: url }, fileName: file_name, mimetype: 'application/octet-stream' }, { quoted: m })
+} catch (e) {
+throw 'Terjadi kesalahan saat memproses permintaan'
 }
-
-async function download(url) {
-	const res = await fetch(url);
-	let $ = cheerio.load(await res.text());
-	const filename = $('img.intro').attr('alt');
-	const mimetype = $('div.list').text().split(' - ')[1].split('\n')[0];
-	const dl = $('#download').attr('href');
-	const up_at = $('.list').eq(2).text().split(':')[1].trim();
-	const uploader = $('.list').eq(1).find('a').eq(0).text().trim();
-	const total_down = $('.list').eq(3).text().split(':')[1].trim();
-
-	const data = await fetch(dl);
-	$ = cheerio.load(await data.text());
-	const scripts = $('script')
-		.map((i, el) => $(el).html())
-		.get()
-		.join('\n');
-	const finalUrlRegex = /https:\\\/\\\/download\d+\.sfile\.mobi\\\/downloadfile\\\/\d+\\\/\d+\\\/[a-z0-9]+\\\/[^\s'"]+\.[a-z0-9]+(\?[^"']+)?/gi;
-	const matches = scripts.match(finalUrlRegex);
-	const download = matches[0].replace(/\\\//g, '/');
-
-	return {
-		filename,
-		mimetype,
-		upload_date: up_at,
-		uploader,
-		total_download: total_down,
-		download,
-	};
 }
+handler.help = ['sfile <url>']
+handler.tags = ['downloader']
+handler.command = /^(sfile)$/i
+
+export default handler
