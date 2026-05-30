9683d54062c56d90706b131d059510fd3d96e592
Hilman <himanackerman@gmail.com>
2026-05-30 07:39:00 +0700

    Anu

--- plugins/info-dashboard.js
+++ plugins/info-dashboard.js
@@ -1,71 +1,72 @@
-let handler = async (m) => {
+let handler = async (m, { conn }) => {
 	let stats = Object.entries(db.data.stats)
 		.map(([key, val]) => {
-			let name = Array.isArray(plugins[key]?.help) ? plugins[key]?.help.join(' , ') : plugins[key]?.help || key;
-			if (/exec/.test(name)) return;
-			return { name, ...val };
+			let name = Array.isArray(plugins[key]?.help)
+				? plugins[key]?.help.join(' , ')
+				: plugins[key]?.help || key
+
+			if (/exec/.test(name)) return
+
+			return { name, ...val }
 		})
-		.filter(Boolean);
-	stats = stats.sort((a, b) => b.total - a.total);
+		.filter(Boolean)
+
+	stats = stats.sort((a, b) => b.total - a.total)
 
 	let handlers = stats
 		.slice(0, 50)
 		.map(({ name, total, last, success, lastSuccess }, i) => {
-			return `*${i + 1}.* *${name}*\n • *Hits* : ${total}\n • *Success* : ${success}\n • *Last Used* : ${getTime(last)}\n • *Last Success* : ${formatTime(lastSuccess)}`;
+			return `*${i + 1}.* *${name}*
+ • *Hits* : ${total}
+ • *Success* : ${success}
+ • *Last Used* : ${getTime(last)}
+ • *Last Success* : ${formatTime(lastSuccess)}`
 		})
-		.join('\n\n');
+		.join('\n\n')
 
-	conn.relayMessage(
-		m.chat,
-		{
-			extendedTextMessage: {
-				text: handlers,
-				contextInfo: {
-					externalAdReply: {
-						title: 'D A S H B O A R D',
-						mediaType: 1,
-						previewType: 1,
-						renderLargerThumbnail: true,
-						thumbnailUrl: 'https://telegra.ph/file/cf4f28ed3b9ebdfb30adc.png',
-						sourceUrl: global.link,
-					},
-				},
-				mentions: [m.sender],
-			},
+	await conn.sendMessage(m.chat, {
+		image: {
+			url: 'https://telegra.ph/file/cf4f28ed3b9ebdfb30adc.png'
 		},
-		{}
-	);
-};
+		caption: handlers,
+		mentions: [m.sender]
+	}, { quoted: m })
+}
 
-handler.help = ['dashboard'];
-handler.command = ['dashboard', 'dash'];
-handler.tags = ['info'];
-export default handler;
+handler.help = ['dashboard']
+handler.command = ['dashboard', 'dash']
+handler.tags = ['info']
+
+export default handler
 
 function formatTime(time) {
-	const date = new Date(time);
-	const month = getMonthName(date.getMonth());
-	const day = date.getDate();
-	const year = date.getFullYear();
+	const date = new Date(time)
+	const month = getMonthName(date.getMonth())
+	const day = date.getDate()
+	const year = date.getFullYear()
 
-	return `${month} ${day}, ${year}`;
+	return `${month} ${day}, ${year}`
 }
 
 function getMonthName(month) {
-	const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	return months[month];
+	const months = [
+		'January', 'February', 'March', 'April', 'May', 'June',
+		'July', 'August', 'September', 'October', 'November', 'December'
+	]
+	return months[month]
 }
 
 function getTime(ms) {
-	var now = parseMs(+new Date() - ms);
-	if (now.days) return `${now.days} days ago`;
-	else if (now.hours) return `${now.hours} hours ago`;
-	else if (now.minutes) return `${now.minutes} minutes ago`;
-	else return `a few seconds ago`;
+	var now = parseMs(+new Date() - ms)
+	if (now.days) return `${now.days} days ago`
+	else if (now.hours) return `${now.hours} hours ago`
+	else if (now.minutes) return `${now.minutes} minutes ago`
+	else return `a few seconds ago`
 }
 
 function parseMs(ms) {
-	if (typeof ms !== 'number') throw 'Parameters must be filled with numbers';
+	if (typeof ms !== 'number') throw 'Parameters must be filled with numbers'
+
 	return {
 		days: Math.trunc(ms / 86400000),
 		hours: Math.trunc(ms / 3600000) % 24,
@@ -73,6 +74,6 @@ function parseMs(ms) {
 		seconds: Math.trunc(ms / 1000) % 60,
 		milliseconds: Math.trunc(ms) % 1000,
 		microseconds: Math.trunc(ms * 1000) % 1000,
-		nanoseconds: Math.trunc(ms * 1e6) % 1000,
-	};
-}
+		nanoseconds: Math.trunc(ms * 1e6) % 1000
+	}
+		}
