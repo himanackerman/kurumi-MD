ddd43a6168b7176c59f3158c7241707e39e3fc5a
Hilman <himanackerman@gmail.com>
2026-05-30 15:33:47 +0700

    Anu

--- plugins/limit.js
+++ plugins/limit.js
@@ -7,33 +7,48 @@ let handler = async (m, { conn }) => {
     throw '❌ User tidak ditemukan'
 
   let user = global.db.data.users[who]
-  let limit = user.premiumTime >= 1 ? '∞ Unlimited' : user.limit
 
   const isDeveloper = global.owner?.some(v => {
     if (Array.isArray(v)) return who.includes(v[0])
     return who.includes(v)
   })
 
+  let limit =
+    isDeveloper
+      ? '∞ Unlimited'
+      : user.premiumTime >= 1
+      ? '∞ Unlimited'
+      : user.limit
+
   let status =
     isDeveloper
-      ? '✨ Developer'
+      ? 'Developer'
       : user.premiumTime >= 1
-      ? '🌸 Premium'
+      ? 'Premium'
       : user.level >= 1000
-      ? '🔥 Elite'
-      : '🌱 Free User'
+      ? 'Elite'
+      : 'Free User'
 
-  let text = `✨ *INFO LIMIT* 
-
-👤 *Username* : ${user.registered ? user.name : conn.getName(who)}
-🏮 *Status*   : ${status}
-🎟️ *Limit*    : ${limit}
-`.trim()
-
-  conn.sendMessage(m.chat, { text }, { quoted: m })
+  await conn.sendMessage(m.chat, {
+    disclaimerText: 'User Information',
+    headerText: `## ${global.namebot}`,
+    contentText: '---',
+    title: 'Limit Information',
+    table: [
+      ['', 'Info'],
+      ['Username', user.registered ? user.name : conn.getName(who)],
+      ['Status', status],
+      ['Limit', `${limit}`]
+    ],
+    noHeading: false
+  }, {
+    quoted: m
+  })
 }
 
 handler.help = ['limit [@user]']
 handler.tags = ['main']
-handler.command = /^(limit)$/i
+handler.command = /^limit$/i
+handler.limit = false
+
 export default handler
