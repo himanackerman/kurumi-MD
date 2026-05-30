7d886789668d9d71b7a72f07cd54d8f23d0caf40
Hilman <himanackerman@gmail.com>
2026-05-30 15:36:53 +0700

    Anu

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,25 +1,51 @@
 let handler = async (m, { conn }) => {
+  try {
+    await m.react('📊')
 
-  let totalFitur = Object.values(global.plugins)
-    .filter(v => v.help && v.tags).length
+    let totalFitur = Object.values(global.plugins)
+      .filter(v =>
+        v.help &&
+        v.tags &&
+        !v.disabled
+      )
+      .length
 
-  let totalCommand = Object.values(global.plugins)
-    .map(v => v.command)
-    .filter(v => v)
-    .map(v => Array.isArray(v) ? v.length : 1)
-    .reduce((a, b) => a + b, 0)
+    let totalCommand = Object.values(global.plugins)
+      .map(v => v.command)
+      .filter(v => v)
+      .map(v =>
+        Array.isArray(v)
+          ? v.length
+          : 1
+      )
+      .reduce((a, b) => a + b, 0)
 
-  let text = `
-Total Fitur   : ${totalFitur}
-Total Command : ${totalCommand}
-`.trim()
+    await m.react('✅')
 
-  await conn.sendMessage(m.chat, { text }, { quoted: m })
+    await conn.sendMessage(m.chat, {
+      disclaimerText: 'Bot Information',
+      headerText: `## ${global.namebot}`,
+      contentText: '---',
+      title: 'Total Features',
+      table: [
+        ['', 'Total'],
+        ['Feature', `${totalFitur}`],
+        ['Command', `${totalCommand}`]
+      ],
+      noHeading: false
+    }, {
+      quoted: m
+    })
+
+  } catch (e) {
+    console.log(e)
+    m.reply('...error.')
+  }
 }
 
 handler.help = ['totalfitur']
 handler.tags = ['info']
-handler.command = ['totalfitur']
+handler.command = /^totalfitur$/i
 handler.limit = false
 
 export default handler
